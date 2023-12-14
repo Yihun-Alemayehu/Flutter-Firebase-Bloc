ab617ee0e7272157e9ffba5ef5dd49d6eabf4944
Yihun Alemayehu <yankure01@gmail.com>
2023-12-14 23:43:11 +0300

    Authentication commit

--- packages/user_repository/lib/src/firabase_user_repository.dart
+++ packages/user_repository/lib/src/firabase_user_repository.dart
@@ -1,5 +1,68 @@
+import 'dart:developer' show log;
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_repository/src/models/my_user.dart';
 import 'package:user_repository/src/user_repo.dart';
 
 class FirebaseUserRepository implements UserRepository {
+  FirebaseUserRepository({FirebaseAuth? firebaseAuth})
+      : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
 
+  final FirebaseAuth _firebaseAuth;
+
+  //Sign Up
+  @override
+  Future<MyUser> signUp(MyUser myUser, String password) async {
+    try {
+      UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: myUser.email,
+        password: password,
+      );
+
+      myUser = myUser.copyWith(
+        id: user.user!.uid,
+      );
+
+      return myUser;
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  //Sign In
+  @override
+  Future<void> signIn(String email, String password) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  //Sign Out
+  @override
+  Future<void> logOut() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  //reset password
+  @override
+  Future<void> resetPassword(String email) async {
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
 }
--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -1,1 +1,12 @@
-abstract class UserRepository {}
+import 'models/my_user.dart';
+
+abstract class UserRepository {
+
+  Future<void> signIn(String email, String password);
+
+  Future<void> logOut();
+
+  Future<MyUser> signUp(MyUser myUser, String password);
+
+  Future<void> resetPassword(String email);
+}
