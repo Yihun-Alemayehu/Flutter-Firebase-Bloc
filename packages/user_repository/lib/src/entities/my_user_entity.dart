cb71cd26a618bcbb44d4d137fd1671de7d551dba
Yihun Alemayehu <yankure01@gmail.com>
2023-12-14 22:55:19 +0300

    user model commit

--- packages/user_repository/lib/src/entities/my_user_entity.dart
+++ packages/user_repository/lib/src/entities/my_user_entity.dart
@@ -1,1 +1,46 @@
-class MyUserEntity {}
+import 'package:equatable/equatable.dart';
+
+class MyUserEntity extends Equatable {
+  final String id;
+  final String name;
+  final String email;
+  final String? picture;
+
+  const MyUserEntity({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.picture,
+  });
+
+  Map<String, Object?> toDocument() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'picture': picture,
+    };
+  }
+
+  static MyUserEntity fromDocument(Map<String, dynamic> doc) {
+    return MyUserEntity(
+      id: doc['id'],
+      name: doc['name'],
+      email: doc['email'],
+      picture: doc['picture'],
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, name, email, picture];
+
+  @override
+  String toString() {
+    return '''UserEntity: {
+      id: $id
+      name: $name
+      email: $email
+      picture: $picture
+      }''';
+  }
+}
--- packages/user_repository/lib/src/models/my_user.dart
+++ packages/user_repository/lib/src/models/my_user.dart
@@ -1,3 +1,65 @@
-class MyUser {
-  
+import 'package:equatable/equatable.dart';
+import 'package:user_repository/src/entities/entities.dart';
+
+class MyUser extends Equatable {
+  final String id;
+  final String name;
+  final String email;
+  final String? picture;
+
+  const MyUser({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.picture,
+  });
+
+  //Empty user
+  static const empty = MyUser(
+    id: '',
+    name: '',
+    email: '',
+    picture: '',
+  );
+
+  MyUser copyWith({
+    String? id,
+    String? name,
+    String? email,
+    String? picture,
+  }) {
+    return copyWith(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      picture: picture ?? this.picture,
+    );
+  }
+
+  //getter to determine whether the currect user is empty
+  bool get isEmpty => this == MyUser.empty;
+
+  //getter to determine whether the currect user is not empty
+  bool get isNotEmpty => this != MyUser.empty;
+
+  MyUserEntity toEntity(){
+    return MyUserEntity(
+      id: id,
+      name: name,
+      email: email,
+      picture: picture,
+    );
+  }
+
+  static MyUser fromEntity(MyUserEntity entity){
+    return MyUser(
+      id: entity.id,
+      name: entity.name,
+      email: entity.email,
+      picture: entity.picture,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, name, email, picture];
 }
