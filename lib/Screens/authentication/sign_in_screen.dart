265cd01c66bab0aa959238df4193d02f20af2541
Yihun Alemayehu <yankure01@gmail.com>
2023-12-16 00:15:56 +0300

    signUp screen commit-01

--- lib/Screens/authentication/sign_in_screen.dart
+++ lib/Screens/authentication/sign_in_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_firebase_bloc/blocs/sign_in/sign_in_bloc.dart';
 import 'package:flutter_firebase_bloc/components/strings.dart';
 import 'package:flutter_firebase_bloc/components/textfield.dart';
 
@@ -13,38 +15,112 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   final _formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
+  final passwordController = TextEditingController();
   String? _errorMsg;
+  bool obscurePassword = true;
+  IconData iconPassword = CupertinoIcons.eye_fill;
+  bool signInRequired = false;
 
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 20,
+      child: Column(children: [
+        const SizedBox(
+          height: 30,
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: MyTextField(
+            controller: emailController,
+            hintText: 'Email',
+            obscureText: false,
+            keyboardType: TextInputType.emailAddress,
+            prefixIcon: const Icon(CupertinoIcons.mail_solid),
+            errorMsg: _errorMsg,
+            validator: (val) {
+              if (val!.isEmpty) {
+                return 'Please fill the field';
+              } else if (!emailRexExp.hasMatch(val)) {
+                return 'Please enter a valid email ';
+              }
+              return null;
+            },
           ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: MyTextField(
-              controller: emailController,
-              hintText: 'Email',
-              obscureText: false,
-              keyboardType: TextInputType.emailAddress,
-              prefixIcon: const Icon(CupertinoIcons.mail_solid),
-              errorMsg: _errorMsg,
-              validator: (val) {
-                if (val!.isEmpty) {
-                  return 'Please fill the field';
-                } else if (!emailRexExp.hasMatch(val)) {
-                  return 'Please enter a valid email ';
-                }
-                return null;
-              },
-            ),
-          )
-        ],
-      ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: MyTextField(
+            controller: passwordController,
+            hintText: 'password',
+            obscureText: obscurePassword,
+            keyboardType: TextInputType.visiblePassword,
+            prefixIcon: const Icon(CupertinoIcons.lock_fill),
+            errorMsg: _errorMsg,
+            validator: (val) {
+              if (val!.isEmpty) {
+                return 'Please fill the field';
+              } else if (!passwordRexExp.hasMatch(val)) {
+                return 'Please enter a valid email ';
+              }
+              return null;
+            },
+            suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    obscurePassword = !obscurePassword;
+                    if (obscurePassword) {
+                      iconPassword = CupertinoIcons.eye_fill;
+                    } else {
+                      iconPassword = CupertinoIcons.eye_slash_fill;
+                    }
+                  });
+                },
+                icon: Icon(iconPassword)),
+          ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        !signInRequired
+            ? SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 60,
+                child: TextButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        context.read<SignInBloc>().add(
+                              SignInRequired(
+                                  email: emailController.text,
+                                  password: passwordController.text),
+                            );
+                      }
+                    },
+                    style: TextButton.styleFrom(
+                        elevation: 3,
+                        backgroundColor: Colors.blueAccent,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(60),
+                        )),
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    )),
+              )
+            : const CircularProgressIndicator(),
+      ]),
     );
   }
 }
