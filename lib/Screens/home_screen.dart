cae708ed731661f8f03ac60be07d053346fe3e9d
Yihun Alemayehu <yankure01@gmail.com>
2023-12-16 20:52:51 +0300

    HomeScreen commit

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -9,13 +9,97 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                context.read<SignInBloc>().add(const SignOutRequired());
-              },
-              icon: const Icon(Icons.logout))
+        title: const Padding(
+          padding: EdgeInsets.only(top: 10),
+          child: Text(
+            'G r a c e L i n k',
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.black,
+              letterSpacing: 2.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: const [
+          // IconButton(
+          //     onPressed: () {
+          //       context.read<SignInBloc>().add(const SignOutRequired());
+          //     },
+          //     icon: const Icon(Icons.logout))
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: CircleAvatar(
+              radius: 35,
+              backgroundImage: AssetImage('assets/dad.jpg'),
+            ),
+          )
         ],
+      ),
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: double.infinity,
+              //height: 400,
+              //color: Colors.blueAccent,
+              child: Column(
+                children: [
+                  const Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: CircleAvatar(
+                          radius: 25,
+                          backgroundImage: AssetImage('assets/dad.jpg'),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'GraceLink',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text('3 minutes'),
+                        ],
+                      ),
+                      Expanded(child: SizedBox()),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.more_vert_rounded,
+                          size: 30,
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    //color: Colors.grey,
+                    child: const Text(
+                      'May the God of hope fill you with all joy and peace as you trust in him, so that you may overflow with hope by the power of the Holy Spirit.',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
