adad74318ad7bbf66bc785576d400a5375191860
Yihun Alemayehu <yankure01@gmail.com>
2023-12-17 21:58:05 +0300

    post model commit

--- packages/post_repository/lib/src/firebase_post_repository.dart
+++ packages/post_repository/lib/src/firebase_post_repository.dart
@@ -1,3 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:post_repository/post_repository.dart';
+import 'package:post_repository/src/models/post.dart';
 import 'package:post_repository/src/post_repo.dart';
+import 'package:uuid/uuid.dart';
 
-class FirebasePostRepository implements PostRepository {}
+class FirebasePostRepository implements PostRepository {
+  final postCollection = FirebaseFirestore.instance.collection('posts');
+
+  @override
+  Future<Post> createPost(Post post) async {
+    try {
+      post.postId = const Uuid().v1();
+      post.timeStamp = DateTime.now();
+
+      await postCollection.doc(post.postId).set(post.toEntity().toDocument());
+      return post;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<Post>> getPost() {
+    try {
+      return postCollection.get().then((value) => value.docs
+          .map((e) => Post.fromEntity(PostEntity.fromDocument(e.data())))
+          .toList());
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
--- packages/post_repository/lib/src/post_repo.dart
+++ packages/post_repository/lib/src/post_repo.dart
@@ -1,1 +1,6 @@
-abstract class PostRepository {}
+import 'package:post_repository/post_repository.dart';
+
+abstract class PostRepository {
+  Future<Post> createPost(Post post);
+  Future<List<Post>> getPost();
+}
