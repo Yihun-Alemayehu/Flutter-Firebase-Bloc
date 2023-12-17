8ad81510736ffb488c842e6d70198e9fa912d8f2
Yihun Alemayehu <yankure01@gmail.com>
2023-12-17 17:46:53 +0300

    post model commti

--- packages/post_repository/lib/src/entities/post_entity.dart
+++ packages/post_repository/lib/src/entities/post_entity.dart
@@ -1,1 +1,46 @@
-class PostEntity {}
+import 'package:user_repository/user_repository.dart';
+
+class PostEntity {
+  String postId;
+  String post;
+  DateTime timeStamp;
+  MyUser myUser;
+
+  PostEntity({
+    required this.postId,
+    required this.post,
+    required this.timeStamp,
+    required this.myUser,
+  });
+
+  Map<String, Object?> toDocument() {
+    return {
+      'postId': postId,
+      'post': post,
+      'timeStamp': timeStamp,
+      'myUser': myUser.toEntity().toDocument(),
+    };
+  }
+
+  static PostEntity fromDocument(Map<String, dynamic> doc) {
+    return PostEntity(
+      postId: doc['postId'],
+      post: doc['post'],
+      timeStamp:DateTime.parse(doc['timeStamp']),
+      myUser: MyUser.fromEntity(MyUserEntity.fromDocument(doc['myUser'])) ,
+    );
+  }
+
+  @override
+  List<Object?> get props => [postId, post, timeStamp, myUser];
+
+  @override
+  String toString() {
+    return '''PostEntity: {
+      postId: $postId
+      post: $post
+      timeStamp: $timeStamp
+      myUser: $myUser
+      }''';
+  }
+}
--- packages/post_repository/lib/src/models/post.dart
+++ packages/post_repository/lib/src/models/post.dart
@@ -1,3 +1,66 @@
+import 'package:user_repository/user_repository.dart';
+
+import '../entities/entities.dart';
+
 class Post {
-  
+  String postId;
+  String post;
+  DateTime timeStamp;
+  MyUser myUser;
+
+  Post({
+    required this.postId,
+    required this.post,
+    required this.timeStamp,
+    required this.myUser,
+  });
+
+  //Empty user
+  static final empty = Post(
+    postId: '',
+    post: '',
+    timeStamp: DateTime.now(),
+    myUser: MyUser.empty,
+  );
+
+  Post copyWith({
+    String? postId,
+    String? post,
+    DateTime? timeStamp,
+    MyUser? myUser,
+  }) {
+    return Post(
+      postId: postId ?? this.postId,
+      post: post ?? this.post,
+      timeStamp: timeStamp ?? this.timeStamp,
+      myUser: myUser ?? this.myUser,
+    );
+  }
+
+  //getter to determine whether the currect user is empty
+  bool get isEmpty => this == Post.empty;
+
+  //getter to determine whether the currect user is not empty
+  bool get isNotEmpty => this != Post.empty;
+
+  PostEntity toEntity() {
+    return PostEntity(
+      postId: postId,
+      post: post,
+      timeStamp: timeStamp,
+      myUser: myUser,
+    );
+  }
+
+  static Post fromEntity(PostEntity entity) {
+    return Post(
+      postId: entity.postId,
+      post: entity.post,
+      timeStamp: entity.timeStamp,
+      myUser: entity.myUser,
+    );
+  }
+
+  // @override
+  // List<Object?> get props => [postId, post, timeStamp, myUser];
 }
