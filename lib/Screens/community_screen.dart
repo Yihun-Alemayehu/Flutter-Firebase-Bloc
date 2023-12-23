cf192c0b9c4db24ce5616e3f77b21c2fe6b654aa
Yihun Alemayehu <yankure01@gmail.com>
2023-12-23 20:50:56 +0300

    community page

--- lib/Screens/community_screen.dart
+++ lib/Screens/community_screen.dart
@@ -6,19 +6,108 @@ class CommunityScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Community page'),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.diversity_3_rounded,
-              size: 100,
-            ),
-            Text('Community page'),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, top: 40, right: 20),
+          child: Column(
+            children: [
+              // Title text
+              const Row(
+                children: [
+                  Text(
+                    'GraceLink',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 3,
+                    ),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.only(right: 20),
+                    child: Icon(
+                      Icons.sensors,
+                      size: 35,
+                    ),
+                  ),
+                ],
+              ),
+
+              Expanded(
+                child: Card(
+                  child: ListView.builder(
+                    itemCount: 20,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Row(
+                            children: [
+                              CircleAvatar(
+                                backgroundImage: AssetImage('assets/dad.jpg'),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Aychew Desalegn',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                      )),
+                                  Text('30 minutes ago'),
+                                ],
+                              ),
+                              Spacer(),
+                              Icon(Icons.more_vert),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Text('This will be the first community post..'),
+                          Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                side: BorderSide.none),
+                            child: const Image(
+                              image: AssetImage('assets/post.png'),
+                            ),
+                          ),
+                          const Row(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Icon(
+                                  Icons.favorite,
+                                  color: Colors.red,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Icon(
+                                  Icons.add_comment_rounded,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Icon(
+                                  Icons.ios_share,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
