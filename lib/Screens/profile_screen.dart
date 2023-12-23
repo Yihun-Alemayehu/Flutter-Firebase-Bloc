de55dcd89b4b8ea800fa35b206c1f91c613eb1cf
Yihun Alemayehu <yankure01@gmail.com>
2023-12-24 00:42:06 +0300

    Profile page

--- lib/Screens/profile_screen.dart
+++ lib/Screens/profile_screen.dart
@@ -6,8 +6,101 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile page'),
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBar(
+            leading: Icon(Icons.sort),
+            actions: [Icon(Icons.more_vert)],
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image(
+                image: AssetImage('assets/copy.jpg'),
+                fit: BoxFit.cover,
+              ),
+              title: Text(
+                'YIHUN ALEMAYEHU',
+                style: TextStyle(
+                  letterSpacing: 3,
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text('@Yihunalemayehu',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text('2.3K Followers'),
+                    Text('159 Followings'),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                    'Flutter developer| Software Engineer| Full-stack developer '),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      color: const Color.fromARGB(255, 55, 126, 250),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Follow',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      color: Colors.black,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Message',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
